880f84d613e4741531f629a4a57c3a3f047a11d5
Paul Morales <paulginga@hotmail.com>
2020-08-20 13:30:43 -0500

    add files

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
+var currentDay = moment().format("dddd, MMM Do");
 
+
+var date = document.createElement("p1")
+date.innerHTML = currentDay
+$("header").append(date)
+
+$(".saveBtn").on("click", function () {
+    console.log(this)
+
+    var textArea = $(this).parent().siblings().find("textarea").val()
+    console.log(textArea)
+
+    var localSt = $(this).parent().siblings().find("textarea").attr("id")
+
+    console.log(localSt)
+
+    var task = {
+        time: localSt,
+        value: textArea
+    }
+
+    localStorage.setItem(localSt, textArea)
+
+})
+
+var task = localStorage.getItem("9")
+if (task) {
+    $("#9").val(task)
+}
+
+var rows = $('.row');
+
+var currentTime = (moment().hour())
+
+
+$(".row").each(function () {
+    var timeId = $(this).find('textarea').attr('id');
+    var idInteger = parseInt(timeId)
+
+    // console.log(idInteger)
+
+    if (idInteger > currentTime) {
+        idInteger.addClass(["past"])
+    }
+    if (idInteger < currentTime) {
+        idInteger.addClass(["future"])
+    }
+    if (idInteger === currentTime) {
+        idInteger.addClass(["present"])
+    }
+
+});
